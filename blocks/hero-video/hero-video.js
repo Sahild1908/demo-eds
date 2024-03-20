6ef12dc72c1263a50c8c578d42cdc08eb8e313f8
Sahil Dhiman <sahil.dhiman@grazitti.com>
2024-03-20 16:18:15 +0530

    update

--- blocks/hero-video/hero-video.js
+++ blocks/hero-video/hero-video.js
@@ -1,15 +1,10 @@
 export default function decorate(block){
 const videoBanner = block.children[0];
-console.log("videoBanner " + videoBanner);
 const heroContent = videoBanner.children[0];
-console.log("heroContent " + heroContent);
 const teaserVideoLink = heroContent.querySelector('a');
-console.log("teaserVideoLink " + teaserVideoLink);
-var anchorTag = document.querySelector('a');
-console.log("anchortag " + anchorTag );
+
 // Create a video element
 var video = document.createElement('video');
-console.log("video " + video );
 // Set the src attribute of the video tag to the href value of the anchor tag
 video.src = teaserVideoLink;
 
