78a090a2aeaf8635da8c2faee35a03faac3e23a7
Sahil Dhiman <sahil.dhiman@grazitti.com>
2024-03-20 16:31:21 +0530

    update

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -6,29 +6,27 @@ import { loadFragment } from '../fragment/fragment.js';
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  const footerMeta = getMetadata('nav');
+  const headerMeta = getMetadata('nav');
   block.textContent = '';
 
   // load footer fragment
   var currentPageUrl = window.location.href;
 
 
-let footerPath;
+let headerPath;
 
 if (currentPageUrl.includes('fr')) {
-    console.log("The URL contains 'FR'.");
-    footerPath = footerMeta.footer || '/fr/nav';
+    headerPath = headerMeta.footer || '/fr/nav';
 } else if (currentPageUrl.includes('en')) {
     console.log("The URL contains 'EN'.");
-    footerPath = footerMeta.footer || '/en/nav';
-    console.log("Value of footerpath: " + footerPath);
+    headerPath = headerMeta.footer || '/en/nav';
 }
 
-const fragment = await loadFragment(footerPath);
+const fragment = await loadFragment(headerPath);
 
   // decorate footer DOM
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  const header = document.createElement('div');
+  while (fragment.firstElementChild) header.append(fragment.firstElementChild);
 
-  block.append(footer);
+  block.append(header);
 }
