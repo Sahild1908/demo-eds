8882ae223a5c8fcc1f86a83c860ccddacbff6207
Sahil Dhiman <sahil.dhiman@grazitti.com>
2024-03-20 18:24:14 +0530

    update

--- blocks/hero-video/hero-video.js
+++ blocks/hero-video/hero-video.js
@@ -7,12 +7,10 @@ const teaserVideoLink = heroContent.querySelector('a');
 var video = document.createElement('video');
 // Set the src attribute of the video tag to the href value of the anchor tag
 video.src = teaserVideoLink;
-//video.poster="https://www.evidentscientific.com/data/Media/home-brand.jpg?rev=FBD0";
-//// Add additional attributes as needed, e.g., controls
-//video.controls = true;
-//video.preload= none;
+
+// Add additional attributes as needed, e.g., controls
+video.controls = true;
 
 // Replace the anchor tag with the video element
 teaserVideoLink.parentNode.replaceChild(video, teaserVideoLink);
-
 }
