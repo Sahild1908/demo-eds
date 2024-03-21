115dfc50db1531f363be21164b221205bd3aded3
Sahil Dhiman <sahil.dhiman@grazitti.com>
2024-03-21 15:32:22 +0530

    update

--- blocks/hero-video/hero-video.js
+++ blocks/hero-video/hero-video.js
@@ -8,15 +8,15 @@ console.log("Teaser Video Link "+ teaserVideoLink);
 
 
 // Create a video element
-//var video = document.createElement('video');
+var video = document.createElement('video');
 //// Set the src attribute of the video tag to the href value of the anchor tag
-//video.src = teaserVideoLink;
+video.src = teaserVideoLink.href;
 //
 //// Add additional attributes as needed, e.g., controls
-//video.controls = true;
+video.controls = true;
 //
 //// Replace the anchor tag with the video element
-//teaserVideoLink.parentNode.replaceChild(video, teaserVideoLink);
-//console.log("Latest Value "+ teaserVideoLink);
+teaserVideoLink.parentNode.replaceChild(video, teaserVideoLink);
+console.log("Latest Value "+ teaserVideoLink);
 
 }
