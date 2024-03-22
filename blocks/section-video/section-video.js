e704e8a9a7d4c8f12623211cfaff48f588178a29
Sahil Dhiman <sahil.dhiman@grazitti.com>
2024-03-22 10:38:47 +0530

    update

--- blocks/section-video/section-video.js
+++ blocks/section-video/section-video.js
@@ -1,5 +1,5 @@
 export default function decorate(block){
-let anchorTag = document.querySelector('.header-video .button-container a');
+let anchorTag = document.querySelector('.section-video .button-container a');
 var video = document.createElement('video');
 video.src = anchorTag.href;
 video.autoplay="autoplay";
