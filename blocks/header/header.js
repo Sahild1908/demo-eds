5df46852444e60c6a2feee6cba92a665e5349a18
Sahil Dhiman <sahil.dhiman@grazitti.com>
2024-03-28 15:25:01 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -9,7 +9,7 @@ import { loadFragment } from '../fragment/fragment.js';
 export default async function decorate(block) {
   const headerMeta = getMetadata('nav');
   block.textContent = '';
-debugger;
+
   var currentPageUrl = window.location.href;
   let headerPath;
   const languageToPath = {
@@ -23,6 +23,7 @@ debugger;
       'pt': '/pt/nav',
       'ru': '/ru/nav'
   };
+  debugger;
 const pathParts = new URL(currentPageUrl).pathname.split('/').filter(part => part !== ''); // Get path parts and remove empty parts
 const desiredPart = pathParts[1]; // Get the second part of the path, which is "it"
 
